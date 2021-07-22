863c3a3bfc474970849cb612337ba6ad109fe510
Josh W <you@example.com>
2021-07-22 09:00:49 -0400

    final changes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,34 +1,34 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update'
 
-import { errorHandler, NotFoundError, currentUser } from "@jbwtickets/common";
+import { errorHandler, NotFoundError, currentUser } from '@jbwtickets/common'
 
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secure: false,
   })
-);
+)
 
-app.use(currentUser);
+app.use(currentUser)
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
 
-app.all("*", async (req, res, next) => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError()
+})
+app.use(errorHandler)
 
-export { app };
+export { app }
